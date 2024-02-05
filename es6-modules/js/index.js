f69cb0d4bc08bdcdfa07df5e86c320f450c4a025
Tim Carlson <timca@arenasports.com>
2024-02-04 19:49:04 -0800

    starter branch for es6

---
+++ es6-modules/js/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+//Slide 7
+import './other.js'
+
+// // Slide 7a
+// import {otherMessage} from './other.js';
+
+//Slide 8
+// import {otherMessage, otherFunc as myFunc} from './other.js';
+// import * as theModule from './other.js'; 
+
+//Slide 10
+import {otherMessage} from './other.js';
+import otherDefault from './other.js';
+
+
+//code executing
+
+// //Slide 6 - see it's working in liveserver
+// console.log("We're live from index.js");
+
+// //Slide 7
+// console.log("Other says: ", otherMessage);
+
+
+//Slide 8
+// console.log("Other says: ", otherMessage);
+// // otherFunc();
+// myFunc();
+
+// console.log(theModule.otherMessage);
+// theModule.otherFunc();
+
+//Slide 10
+console.log("Other says: ", otherMessage);
+otherDefault();
+
---
+++ es6-modules/js/other.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log("executing other.js");
+
+// //Slide 7a
+// const otherMessage = "I'm other!";
+
+// //Slide 8
+// export const otherMessage = "I'm other!";
+
+// export function otherFunc() {
+//     console.log("Text from 'otherFunc()' ");
+// }
+
+//Slide 10 default example
+export const otherMessage = "I'm other!";
+
+export default function otherFunc() {
+    console.log("Text from 'otherFunc()' ");
+}
+
+
+ 
